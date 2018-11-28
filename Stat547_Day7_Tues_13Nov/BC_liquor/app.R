37d1e00286a3f7571bfd4984b62aeb891035b332
Sarafa <iyaniwura@math.ubc.ca>
2018-11-27 20:12:31 -0800

    upload files

--- Stat547_Day7_Tues_13Nov/BC_liquor/app.R
+++ Stat547_Day7_Tues_13Nov/BC_liquor/app.R
@@ -1,32 +1,51 @@
 library(shiny)
+library(tidyverse)
 
 
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
+unique(bcl$Type)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
       titlePanel("BC liqour price app",
                 windowTitle = "BCL app"),
-      
+
       sidebarLayout(
-        sidebarPanel("This text is in the sidebar."),
+        sidebarPanel(sliderInput("priceInput", "Select your desired price range.",
+                                 min = 0, max = 100, value = c(15, 30), pre="$"),
+                     radioButtons("typeInput", "Select you alcoholic beverage types",
+                                  choices =  c("BEER","REFRESHMENT","SPIRIT","WINE"),
+                                  selected = "WINE")),
         mainPanel(
           plotOutput("price_hist"),
           tableOutput("bcl_data"),
           plotOutput("Alcohol_hist")
-          
+
         )
       )
-  
 
-   
+
+
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$price_hist <-renderPlot(ggplot2::qplot(bcl$Price))
-  output$bcl_data <- renderTable(head(bcl))
+    observe(print(input$priceInput))
+    bcl_filtered <- reactive({
+        bcl %>%
+            filter(Price < input$priceInput[2],
+                   Price > input$priceInput[1],
+                   Type == input$typeInput)
+    })
+  output$price_hist <-renderPlot({
+     bcl_filtered() %>%   # bcl_filtere is now a function and not a data frame
+       ggplot(aes(Price)) + geom_histogram()
+      })
+  output$bcl_data <- renderTable({
+      bcl_filtered()
+      })
+
   output$Alcohol_hist <-renderPlot(ggplot2::qplot(bcl$Alcohol_Content))
 
 
